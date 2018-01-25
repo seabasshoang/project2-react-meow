e476d581d99c122e724fa17e4dbfce45f6544270
sylvia hoang <sylviahoang@hotmail.co.uk>
2018-01-25 21:16:04 +0000

    extracted footer to separate component then imported it back in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import MyHeader from './components/MyHeader';
 import MyNav from './components/MyNav';
+import MyFooter from './components/MyFooter';
 
 class App extends Component {
   render() {
@@ -38,10 +39,7 @@ class App extends Component {
             <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
         </div>
 
-        <footer>
-            <h2>Footer</h2>
-            <a href="#top">Back to Top</a>
-        </footer>
+        <MyFooter/>
 
       </div>
     );
---
+++ src/components/MyFooter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MyFooter extends React.Component{
+    render(){
+        return(
+            <div>
+                <footer>
+                    <h2>Footer</h2>
+                    <a href="#top">Back to Top</a>
+                </footer>
+            </div>
+        )
+    }
+}
+export default MyFooter;
