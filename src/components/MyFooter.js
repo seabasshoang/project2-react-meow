734598b2c1b2a8905bc212915db076ade39e2cf6
sylvia hoang <sylviahoang@hotmail.co.uk>
2018-01-31 22:07:06 +0000

    removed proptype from MyFooter as it wasnt needed

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+
 
 class MyFooter extends React.Component{
     render(){
@@ -15,6 +15,3 @@ class MyFooter extends React.Component{
 }
 export default MyFooter;
 
-MyFooter.propTypes = {
-    href: PropTypes.string
-}
